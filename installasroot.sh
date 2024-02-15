1e0aa3067c780f8d539e0b3fbfd611c3bffff9d5
John Radley <jradxl@gmail.com>
2024-02-15 15:58:20 +0000

    Update installasroot.sh

--- installasroot.sh
+++ installasroot.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-#Download this file using the following URL:
-#  curl -fsSl https://raw.githubusercontent.com/jradxl/dotfiles/main/install.sh > install.sh
+#set -eu
 
+#Download this file using the following URL:
+#  curl -fsSl https://raw.githubusercontent.com/jradxl/dotfiles/main/installasroot.sh > install.sh
+#  chmod +x install.sh
+# ./install.sh
 
 PUBLICKEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3YXTN6G2zLiwneZgtlPsAAg1uwj8OcB3vVmph4paM2"
+
+# IMPORTANT
+#Not stored in Git Repo. Add manually before running.
+TAILSCALEAUTHKEY="Empty"
+
+if [[ "$(id -u)" != "0" ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if [[ "$TAILSCALEAUTHKEY" == "Empty" ]]; then
+   echo "Edit this script to add a Tailscale Auth Key before running." 1>&2
+   exit 1
+fi
 
 echo "Installing..."
 touch /root/.ssh/authorized_keys
@@ -39,12 +56,13 @@ isDesktopOrServer
 ##Install packages common to both Server and Desktop
 apt-get --no-install-recommends --quiet --yes install curl wget openssh-server \
         git mtools btrfs-progs build-essential libxt-dev libpython3-dev libncurses-dev \
-        htop glances btop keychain jq python3-venv python3-pip
+        htop glances btop keychain jq python3-venv python3-pip net-tools dirmngr gnupg \
+        gawk bridge-utils smartmontools
 
 if [[ "$LOCATION" == "desktop" ]]; then
   echo "Installing for Desktop..."
   apt-get --no-install-recommends --quiet --yes install  terminator gparted libgtk-3-dev \
-          chromium-browser firefox
+          chromium-browser firefox gsmartcontrol
 fi
 
 #if [[ "$LOCATION" == "server" ]]; then
@@ -58,4 +76,22 @@ username=jovyan
 password=jovyan
 adduser --gecos "" --disabled-password "$username"
 chpasswd <<<"$username:$password"
-adduser "$username" sudo
+adduser "$username" sudo || echo "Already present."
+
+if [[ ! $(which tailscale) ]]; then
+  echo "Installing Tailscale..."
+  curl -fsSL https://tailscale.com/install.sh | sh
+else
+ echo "Tailscale already installed."
+fi
+
+if [[ $(tailscale status | grep "Logged" ) ]]; then
+    echo "Activating Tailscale with Auth Key"
+    tailscale up --auth-key "$TAILSCALEAUTHKEY"
+    tailscale status
+else
+  echo "Tailscale is up"
+fi
+
+exit 0
+
