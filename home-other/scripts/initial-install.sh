b682b357f4818b6e392658e69bd14bafa7d8413c
John Radley <jradxl@gmail.com>
2024-08-30 18:33:20 +0200

    Updating

--- home-other/scripts/initial-install.sh
+++ home-other/scripts/initial-install.sh
@@ -2,6 +2,14 @@
 
 echo "Starting Initial Install..."
 
+echo "Checking for Curl. May ask for superuser access."
+if [[ $(command -v curl) ]]; then
+    echo "Curl is present, continuing..."
+else
+    echo "Curl is NOT present, installing..."
+    sudo apt-get -y install curl
+fi
+
 if [[ $(command -v swiftly) ]]; then
     ##All Set
     echo "Swiftly set up, continuing..."
@@ -34,6 +42,28 @@ else
 fi
 
 sudo apt-get -y install terminator git nmap net-tools curl wget iproute2 apt-utils age vim pipx rsync
+sudo apt-get -y install liblz4-dev libssl-dev libzstd-dev libxxhash-dev libacl1-dev
+
+#Borg is installed as root user uisng pipx. Therefore must be run as root
+#for the venv to work
+if [[ $(sudo -u root /root/.local/bin/borg --version) ]]; then
+    echo "Borg backup is present"
+else
+    echo "Installling Borg backup using pipx"
+    sudo pipx install borgbackup
+fi
+sudo -u root /root/.local/bin/borg --version
+
+#Borgmatic is installed as root user uisng pipx. Therefore must be run as root
+#for the venv to work
+if [[ $(sudo -u root /root/.local/bin/borgmatic --version) ]]; then
+    echo "Borgmatic is present"
+else
+    echo "Installling Borgmatic using pipx"
+    sudo pipx install borgmatic
+fi
+sudo -u root /root/.local/bin/borgmatic --version
+
 
 if [[ $(command -v chezmoi ) ]]; then
     echo "Chezmoi already installed."
@@ -55,17 +85,24 @@ git config --global user.email
 git config --global user.name "John Radley"
 git config --global user.name
 
-echo "Install the Github Private Key and Y to continue?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit;;
-    esac
-done
+
+if [[ -f "$HOME/.github.configured" ]]; then
+    echo "Github Private Key setup OK"
+else
+    echo "Github Private Key NOT setup."
+  echo "Install the Github Private Key and Y to continue?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) break;;
+            No ) exit;;
+        esac
+    done
+    touch "$HOME/.github.configured"
+fi
 
 echo "Test Github access. May ask for passphrase."
 if [[ $(ssh git@github.com) == 1 ]]; then
-    echo "Github access failed."
+    echo "Github access failed. Please fix and re-run this script."
     exit 1
 fi
 
@@ -74,36 +111,55 @@ if [[ ! -f "$HOME/.chezmoi-pac" ]]; then
     echo 'export CHEZMOI_GITHUB_ACCESS_TOKEN=""' > "$HOME/.chezmoi-pac"
 fi
 
-exit 0
-
 CURENTDIR=$(pwd)
-CHEZMOISRC=$(chezmoi source-path)
+
+CHEZMOISRC1=$(chezmoi source-path) #This includes home if configured
+CHEZMOISRC2=$(chezmoi dump-config | grep sourceDir | sed 's/[^,:]*://g' | sed 's/,//g' | sed 's/^[[:space:]]*//g' | sed 's/\"//g')
+
 if [[ -f "$HOME/.github.configured" ]]; then
-    echo "Github ready..."
-    chezmoi cd
+    echo "Github ready... Attempting to change Chezmoi repo to git access"
+    #NOT USED as opens new shell chezmoi cd
+    cd "$CHEZMOISRC2"
+    echo "Chezmoi Repo Path: <$(pwd)>"
     git remote set-url origin git@github.com:jradxl/dotfiles.git
+    #echo "GIT1: <$?>"
     git config --get remote.origin.url
+    #echo "GIT2: <$?>"
     cd "$CURENTDIR"
-    touch "$HOME/.github.configured"
-else
-    echo "Github Dotfiles Repo not ready"
-    exit 1
 fi
 
+if [[ $(command -v rye) ]]; then
+    echo "Rye already installed"
+else
+    echo "Installing Rye"
+    curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes"  bash
+    rye --version
+    echo "3.11" > .python-version
+    rye fetch
+    mkdir -p ~/.local/share/bash-completion/completions
+    rye self completion > ~/.local/share/bash-completion/completions/rye.bash
+fi
 
+if [[ -d "$HOME/.nvm" ]]; then
+    echo "NVM already installed"
+else
+    echo "Installing NVM"
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+fi
+. "$HOME/.nvm/nvm.sh"
+nvm --version
+
+if [[ $(command -v node) ]]; then
+    echo "NODE already installed"
+else
+    echo "Installing NODEjs"
+    nvm install node
+fi
+node --version
 
 exit 0
 
-curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes"  bash
-rye --version
-mkdir -p ~/.local/share/bash-completion/completions
-rye self completion > ~/.local/share/bash-completion/completions/rye.bash
-
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-nvm --version
-nvm install node
-
-
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -f https://zed.dev/install.sh | sh
 
 ##
@@ -125,7 +181,6 @@ wget $($BWSPATH)
 unzip "$HOME/SecretsManager/$FILENAME"
 mv bws "$HOME/.local/bin"
 cd $CURENTDIR
-
 
 exit 1
 
