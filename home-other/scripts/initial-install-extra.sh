18c804e19f64261cab48893da1568308dd949936
John Radley <jradxl@gmail.com>
2024-09-02 20:00:34 +0200

    Updating initial-install-extra.sh

--- home-other/scripts/initial-install-extra.sh
+++ home-other/scripts/initial-install-extra.sh
@@ -16,96 +16,14 @@ fi
 
 sudo systemctl daemon-reload
 
-## Don't add Pipx, Borg and Borgmatic from Ubuntu repositories
-sudo apt-get -y install build-essential pluma caja terminator keychain git nmap net-tools curl wget iproute2 apt-utils age vim rsync bison qemu-guest-agent spice-vdagent
-sudo apt-get -y install liblz4-dev libssl-dev libzstd-dev libxxhash-dev libacl1-dev
-
 ### CHEZMOI ###
 if [[ $(command -v chezmoi ) ]]; then
-    echo "Chezmoi already installed."
+    echo "Good, Chezmoi is already installed."
 else
-    echo "Installing Chezmoi to ~/.local/bin to use existing PATHs" 
-    sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin   
-fi
-
-##Add to current shell
-export PATH="$HOME/.local/bin:$PATH"
-echo "$PATH"
-
-if [[ -d "$HOME/.local/share/chezmoi" ]]; then
-    echo "Chezmoi Dotfiles repo already exists."
-else
-    echo "Getting existing Dofiles repo..."
-    chezmoi init https://github.com/jradxl/dotfiles.git
-fi
-
-chezmoi --version
-echo "Applying Chezmoi updates to the underlying files"
-echo "CAREFUL: Do you want to force the application of Chezmoi updates?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes )
-			echo "Appling Chezmod updates..."
-			chezmoi apply --force
-			break
-			;;
-        No )
-			echo "Chezmod updates not applied..."
-			break
-			;;
-    esac
-done
-echo "Finished Chezmoi updating..."
-
-#Set up Github
-git config --global user.email "jradxl@gmail.com"
-git config --global user.email
-git config --global user.name "John Radley"
-git config --global user.name
-
-if [[ -f "$HOME/.github.configured" ]]; then
-    echo "Github Private Key setup OK"
-else
-    echo "Github Private Key NOT setup."
-    echo "Install the Github Private Key and Y to continue?"
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes ) break;;
-            No ) exit;;
-        esac
-    done
-    touch "$HOME/.github.configured"
-fi
-
-echo "Test Github access. May ask for passphrase."
-if [[ $(ssh git@github.com) == 1 ]]; then
-    echo "Github access failed. Please fix and re-run this script."
+    echo "Chezmoi needs to be installed first. Run ./initial-install.sh" 
     exit 1
 fi
 
-if [[ ! -f "$HOME/.chezmoi-pac" ]]; then
-    echo "Update .chezmoi-pac when convenient."
-    echo 'export CHEZMOI_GITHUB_ACCESS_TOKEN=""' > "$HOME/.chezmoi-pac"
-fi
-
-CURENTDIR=$(pwd)
-
-CHEZMOISRC1=$(chezmoi source-path) #This includes home if configured
-CHEZMOISRC2=$(chezmoi dump-config | grep sourceDir | sed 's/[^,:]*://g' | sed 's/,//g' | sed 's/^[[:space:]]*//g' | sed 's/\"//g')
-
-if [[ -f "$HOME/.github.configured" ]]; then
-    echo "Github ready... Attempting to change Chezmoi repo to git access"
-    #NOT USED as opens new shell chezmoi cd
-    cd "$CHEZMOISRC2"
-    echo "Chezmoi Repo Path: <$(pwd)>"
-    git remote set-url origin git@github.com:jradxl/dotfiles.git
-    #echo "GIT1: <$?>"
-    git config --get remote.origin.url
-    #echo "GIT2: <$?>"
-    cd "$CURENTDIR"
-fi
-
-### End Chezmoi ###
 
 ### RYE for Python ###
 
@@ -122,6 +40,7 @@ else
 fi
 ## RYE End ###
 
+exit 0
 
 ### NVM and NODE ###
 #This is a fussy installer. .nvm dir needs to pre exist.
