fc2434a1614b94be2f85fbcf49fc2f02b721aac0
John Radley <jradxl@gmail.com>
2024-09-07 14:50:33 +0200

    Updating install-docker.sh

--- home-other/scripts/install-docker.sh
+++ home-other/scripts/install-docker.sh
@@ -3,6 +3,10 @@
 #https://docs.docker.com/engine/security/rootless/
 #INFO sudo machinectl shell $USER@ or use ssh
 
+echo "Docker ROOTFUL and optional ROOTLESS install"
+
+echo "Test Current Running method: $(docker info 2>/dev/null  | grep "Docker Root Dir" )"
+
 # Remove Older packages
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
@@ -20,15 +24,25 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get -y update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras slirp4netns
 
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+sudo systemctl enable --now docker.service
+sudo systemctl enable --now containerd.service
+
+if [ $(getent group docker) ]; then
+  echo "Docker group already exists."
+else
+  echo "Adding Docker group."
+  sudo groupadd docker
+fi
+
+unset DOCKER_HOST
+sudo docker context use default
 
 #Rootful test
 sudo docker run hello-world
 
-echo "NOTE: Do you want to test Docker as a Non-Root user. This account?"
+echo "NOTE: Do you want to test ROOTFUL Docker as a Non-Root user. This account?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
@@ -42,18 +56,22 @@ select yn in "Yes" "No"; do
     esac
 done
 
-sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
-sudo chmod g+rwx "$HOME/.docker" -R
+if [[ $(getent group docker | grep -w $USER) ]]; then
+    echo "$USER is already a member of the Docker group."
+else
+  sudo usermod -aG docker $USER
+  echo "$USER added to Docker group. This script will now exit so you can open a new terminal."
+  exit 0
+fi
 
-#Rootful as Non-Root User
-sudo groupadd docker
-#sudo adduser $USER
-#sudo adduser $USER sudo
-sudo usermod -aG docker $USER
-newgrp docker
+unset DOCKER_HOST
+
+#Rootful test as non-root user
 docker run hello-world
 
-echo "NOTE: Do you want to continue install as Rootless?"
+echo ""
+echo "Docker ROOTFUL is complete. See both 'docker run hello-world' above."
+echo "Do you want to continue to enable ROOTLESS docker:"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
@@ -68,9 +86,36 @@ select yn in "Yes" "No"; do
 done
 
 sudo systemctl disable --now docker.service docker.socket
-sudo rm /var/run/docker.sock
+sudo rm -f /var/run/docker.sock
+#sudo systemctl stop docker.service
+#sudo systemctl stop containerd.service
+#sudo systemctl disable docker.service
+#sudo systemctl disable containerd.service
 
-/usr/bin/##dockerd-rootless-setuptool.sh install
+#Just in case...
+if [[ -d /home/"$USER"/.docker ]]; then
+    sudo chown -R "$USER":"$USER" /home/"$USER"/.docker
+    sudo chmod -R g+rwx "$HOME/.docker"
+fi
 
-exit 0
+if [[ -f /usr/bin/dockerd-rootless-setuptool.sh ]]; then
+    echo "Checking Docker Rootless conditions are met."
+    /usr/bin/dockerd-rootless-setuptool.sh check
+    if [[ "$?" == 0 ]]; then
+        echo "Success: Now starting ROOTLESS setup..."
+         /usr/bin/dockerd-rootless-setuptool.sh install
+    else
+        echo "Error: Conditions unmet."
+        exit 1
+    fi
+else
+    echo "The dockerd-rootless-setuptool.sh does not appear to be available."
+    exit 1
+fi
+
+sudo loginctl enable-linger "$USER"
+
+docker run hello-world
+
+echo "ROOTLESS setup completed"
 
