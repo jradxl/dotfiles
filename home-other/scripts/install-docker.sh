7b9b1e6b0996cc32039cb0bfae782ad045e01d73
John Radley <jradxl@gmail.com>
2024-09-07 14:58:07 +0200

    Updating install-docker.sh

--- home-other/scripts/install-docker.sh
+++ home-other/scripts/install-docker.sh
@@ -11,8 +11,8 @@ echo "Test Current Running method: $(docker info 2>/dev/null  | grep "Docker Roo
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg uidmap dbus-user-session systemd-container
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg uidmap dbus-user-session systemd-container
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
