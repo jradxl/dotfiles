ad0b7ff76e7178c297d26736680165ca85e394a3
John Radley <jradxl@gmail.com>
2024-09-14 23:57:32 +0200

    Updating Incus script

--- home-other/incus/incus-set-static-address.sh
+++ home-other/incus/incus-set-static-address.sh
@@ -4,9 +4,31 @@ INSTANCE=""
 DESIRED_STATIC_IP="10.155.89.215/24"
 NETPLAN_FILE="10-netplan.yaml"
 
-echo "Set a Static IP to an Instance connected to the incusbr0 interface."
+echo "Sets a Static IP to an Instance connected to the incusbr0 interface."
 echo "Use this script with care..."
 echo ""
+
+############ Function ###########
+#Returns 0 if valid, 1 if not
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+############ Function End ###########
+
+############ MAIN ###########
 
 if [[ -z "$1" ]]; then
     echo "Usage: incus-set-static-address.sh <container>"
@@ -84,9 +106,48 @@ echo "Requested IP address with Mask: $DESIRED_STATIC_IP"
 NETPLAN_LIST=$(incus exec "$INSTANCE" -- ls /etc/netplan)
 #echo "Files found in /etc/netplan: $NETPLAN_LIST"
 
+##Obtain User's choice of IP address.
 
-OLD_IFS=$IFS
-IFS=$'\n'
+echo "Using the information presented about, make your choice of Static IP address."
+echo "Notice the configurated DHCP IP range shown above. There is no check your choice"
+echo " does not overlap the range."
+
+LOOP=1
+VALID=0
+
+while ((LOOP <= 5)) ; do
+    echo "Write your desired IP address, including netmask in CIDR notation:"
+    read -r line
+    IP=$(echo $line | cut -d/ -f1)
+    MASK=$(echo $line | awk -F/ '{ print $2}')
+    if [[ -z $MASK ]]; then
+        echo "The netmask must be provided."
+    else
+        ((VALID++))
+    fi
+    valid_ip $IP
+    if [[ $? == 0 ]]; then
+        ((VALID++))
+    fi
+    if [[ $VALID == 2 ]]; then
+        break
+    fi
+    echo "IP incorrect. Try again or Cntrl-C to quit"
+    ((LOOP++))
+    VALID=0
+done
+echo $loop
+if [[ $loop > 5 ]]; then
+    echo "Script gave up after 5 retries."
+    exit 1
+fi
+
+DESIRED_STATIC_IP=$IP/$MASK
+echo "IP address is OK."
+echo "Continuing with: $DESIRED_STATIC_IP"
+
+#OLD_IFS=$IFS
+#IFS=$'\n'
 
 #Incus Delete only actions one file
 for i in $(incus exec "$INSTANCE" -- ls /etc/netplan); do
