0d12c2f6274f6cc674eca316f773f119de6f7af6
John Radley <jradxl@gmail.com>
2025-06-04 19:47:06 +0100

    Add home-other/scripts/install-hishtory.sh Update home-other/scripts/run-once.sh Update home/.bashrc

---
+++ home-other/scripts/install-hishtory.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Checking on HISHTORY..."
+
+if [[ $(command -v hishtory) ]];then
+    echo "HISHTORY already installed, checking for upgrade..."
+    current_hishtory="$(hishtory --version | awk '{print $3}')"
+    echo "CURRENT: $current_hishtory"
+else
+    echo "HISHTORY not found, installing..."
+    current_hishtory=""
+fi
+
+latest_hishtory="v$(lastversion https://github.com/ddworken/hishtory)"
+echo "LATEST: $latest_hishtory"
+
+if [[ "$current_hishtory" == "$latest_hishtory" ]]; then
+    echo "HISHTORY already the latest version."
+    exit 0
+else
+    echo "Installing or Upgrading HISHTORY..."
+fi
+
+curl https://hishtory.dev/install.py | python3 -
+echo "<$?>"
+
+echo "Removing modifications to .bashrc. PLEASE CHECK!"
+sed -i '\|^# Hishtory Config:$|d'                          "$HOME/.bashrc"
+sed -i '\|^source /home/jradley/.hishtory/config.sh$|d'    "$HOME/.bashrc"
+sed -i '\|^export PATH="$PATH:/home/jradley/.hishtory"$|d' "$HOME/.bashrc"
+echo "Done"
+
+exit 0
+
--- home-other/scripts/run-once.sh
+++ home-other/scripts/run-once.sh
@@ -153,6 +153,38 @@ check-flatpaks() {
     fi
 }
 
+check-hishtory() {
+    ## SAME as install-hishtory.sh
+    echo "Checking on HISHTORY..."
+    if [[ $(command -v hishtory) ]];then
+        echo "HISHTORY already installed, checking for upgrade..."
+        current_hishtory="$(hishtory --version | awk '{print $3}')"
+        echo "CURRENT: $current_hishtory"
+    else
+        echo "HISHTORY not found, installing..."
+        current_hishtory=""
+    fi
+
+    latest_hishtory="v$(lastversion https://github.com/ddworken/hishtory)"
+    echo "LATEST: $latest_hishtory"
+
+    if [[ "$current_hishtory" == "$latest_hishtory" ]]; then
+        echo "HISHTORY already the latest version."
+        exit 0
+    else
+        echo "Installing or Upgrading HISHTORY..."
+    fi
+
+    curl https://hishtory.dev/install.py | python3 -
+    echo "<$?>"
+
+    echo "Removing modifications to .bashrc. PLEASE CHECK!"
+    sed -i '\|^# Hishtory Config:$|d'                          "$HOME/.bashrc"
+    sed -i '\|^source /home/jradley/.hishtory/config.sh$|d'    "$HOME/.bashrc"
+    sed -i '\|^export PATH="$PATH:/home/jradley/.hishtory"$|d' "$HOME/.bashrc"
+    echo "Done"
+}
+
 echo ""
 echo "####### Executing the RUN-ONCE script..."
 
@@ -169,3 +201,4 @@ check-flatpaks
 echo "####### RUN-ONCE script finished. #######"
 echo ""
 exit 0
+
