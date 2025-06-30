20a89a6de21826a9f365db7d13beb39a335680ea
John Radley <jradxl@gmail.com>
2025-06-30 19:30:21 +0100

    Update N3N

--- home-other/n3n-vpn/install-n3nvpn.sh
+++ home-other/n3n-vpn/install-n3nvpn.sh
@@ -188,6 +188,8 @@ check-for-env() {
 
 install-n3n() {
     sudo useradd -r -M -s /usr/sbin/nologin n3n
+    ##Remove a failed build
+    sudo -rf "$REPODEST"
     mkdir -p "$REPODEST"
     cd "$REPODEST"
     git clone "$GITREPO"
