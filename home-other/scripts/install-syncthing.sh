8f28e9c7e0e6eb4ebe5260c8a633c8a1402cb0f0
John Radley <jradxl@gmail.com>
2025-07-12 11:56:24 +0100

    Updating Syncthing

--- home-other/scripts/install-syncthing.sh
+++ home-other/scripts/install-syncthing.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
+echo "Installing Syncthing and MMV. Will ask for SUDO password."
+if [[ ! $(command -v mmv) ]]; then
+    sudo apt-get install -y mmv
+else
+    echo "Good! MMV already installed"
+fi
 
-mkdir -p /etc/apt/keyrings
+#RET=$(dpkg -l apt-transport-https | grep "un")
+#echo "RET: $RET"
 
-curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+if [[ ! $(dpkg -s apt-transport-https 2>/dev/null) ]]; then
+    sudo apt-get install -y apt-transport-https
+fi
+
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 
-apt update
+sudo apt-get update
+sudo apt-get -y install syncthing
 
-apt install syncthing
+echo ""
+echo "Assuming installing for youself, $USER"
+sudo systemctl status syncthing@$USER
+    
+if [[ ! -f /etc/systemd/system/syncthing@.service ]]; then
+    sudo mcp "/lib/systemd/system/syncthing*" /etc/systemd/system
+    sudo systemctl daemon-reload
+else
+    echo ""
+    echo "/etc/systemd/system/syncthing@.service already present, not overwrting"
+fi
 
-sync
+exit 0
 
-cp /lib/systemd/system/syncthing* /etc/systemd/system
-
-systemctl daemon-reload
