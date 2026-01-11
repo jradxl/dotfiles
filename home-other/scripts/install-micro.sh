7f6bff170d12467845f8d940be53af41b120a936
John Radley <jradxl@gmail.com>
2026-01-11 14:47:39 +0000

    Update install-micro.sh

--- home-other/scripts/install-micro.sh
+++ home-other/scripts/install-micro.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# wget https://github.com/zyedidia/micro/releases/download/v2.0.14/micro-2.0.14-linux64-static.tgz.sha
-# wget https://github.com/zyedidia/micro/releases/download/v2.0.14/micro-2.0.14-linux64-static.tgz
+# wget https://github.com/zyedidia/micro/releases/download/v2.0.15/micro-2.0.15-linux64.tar.gz
+# wget https://github.com/zyedidia/micro/releases/download/v2.0.15/micro-2.0.15-linux64-static.tar.gz
+# wget https://github.com/zyedidia/micro/releases/download/v2.0.15/micro-2.0.15-linux64-static.tar.gz.sha
 # gmcr="$(curl https://getmic.ro)" && [ $(echo "$gmcr" | shasum -a 256 | cut -d' ' -f1) = 45e188ef0d5300cb04dcdece3934fa92f47b7581125c615a8bfae33ac7667a16 ] && echo "$gmcr" | sh
 
 echo "Installing or upgrading the Micro Editor"
@@ -10,6 +11,7 @@ LATEST_VERSION="NO"
 
 if [[ $(command -v lastversion)  ]]; then
 	LATEST_VERSION=$(lastversion https://github.com/zyedidia/micro)
+	#Returns eg, 2.0.15
 else
 	echo "Please ensure Pipx and Lastversion are installed"
 	exit 1
@@ -21,6 +23,7 @@ if [[ "$LATEST_VERSION" == "NO"  ]]; then
 fi
 
 CURRENT_VERSION=$(micro --version | awk '/Version/ {print $2}')
+# Returns e.g 2.0.14
 
 echo "CURRENT: $CURRENT_VERSION"
 echo "LATEST: $LATEST_VERSION"
@@ -37,7 +40,7 @@ if [[ "$RET" == "0" ]]; then
     sudo apt purge -y micro
 fi
 
-FILENAME="micro-$LATEST_VERSION-linux64-static.tgz"
+FILENAME="micro-$LATEST_VERSION-linux64-static.tar.gz"
 URL="https://github.com/zyedidia/micro/releases/download/v$LATEST_VERSION/$FILENAME"
 
 $(cd /tmp && rm -f $(compgen -G "/tmp/micro*") )
@@ -51,7 +54,7 @@ popd > /dev/null
 if [[ "$RET" == 0 ]]; then
 	echo "Download checksum success"
 	$(cd /tmp && tar xf "$FILENAME")
-	#sudo cp /tmp/micro-"$LATEST_VERSION"/micro /usr/locabin
+	#sudo cp /tmp/micro-"$LATEST_VERSION"/micro /usr/local/bin
 	cp /tmp/micro-"$LATEST_VERSION"/micro "$HOME"/.local/bin
 else
 	echo "Download checksum fails."
@@ -62,3 +65,4 @@ micro --version
 echo "RUN hash -r in your shell"
 
 exit 0
+
