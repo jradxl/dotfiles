ce6d7a5c341be576caae425aed6da211eaf4afe4
John Radley <jradxl@gmail.com>
2026-06-29 16:09:11 +0100

    Update Install GOPASS

--- home-other/scripts/install-gopass.sh
+++ home-other/scripts/install-gopass.sh
@@ -12,7 +12,7 @@ case "$ARCH" in
 esac
 
 #source /etc/os-release
-echo "Installing GOPASS for $ARCH"
+echo "Installing PASS and GOPASS for $ARCH"
 
 curl https://packages.gopass.pw/repos/gopass/gopass-archive-keyring.gpg | sudo tee /usr/share/keyrings/gopass-archive-keyring.gpg >/dev/null
 cat << EOF | sudo tee /etc/apt/sources.list.d/gopass.sources
@@ -25,7 +25,29 @@ Signed-By: /usr/share/keyrings/gopass-archive-keyring.gpg
 EOF
 
 echo ""
+sudo apt-get update -y
 
-sudo apt update && sudo apt install -y gopass gopass-archive-keyring
+echo "Installing GPG, GPG-AGENT and PINENTRY-TTY..."
+sudo apt-get install -y gpg gpg-agent pinentry-tty
+
+echo "Installing GOPASS"
+sudo apt-get install -y gopass gopass-archive-keyring
+
+mkdir  -p "$HOME/.gnupg"
+chmod 700 "$HOME/.gnupg"
+#gpg.conf
+cat << EOF | tee "$HOME/.gnupg"
+pinentry-mode loopback
+EOF
+
+#gpg-agent.conf 
+cat << EOF | tee "$HOME/.gnupg"
+#pinentry-program /usr/bin/pinentry-curses
+pinentry-program /usr/bin/pinentry-tty
+allow-loopback-pinentry
+EOF
+
+gpg --list-keys
+gpg -K
 
 exit 0
